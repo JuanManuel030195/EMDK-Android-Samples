f184aa6ee89c6bd942e4e7ba7be93decdb4e92a0
JuanManuel030195 <manuelgarcia031@outlook.com>
2024-01-27 10:15:55 -0600

    Building class added

---
+++ BarcodeSample1/app/src/main/java/com/symbol/barcodesample1/Building.java
@@ -0,0 +1,25 @@
+package com.symbol.barcodesample1;
+
+public class Building {
+    private final int id;
+    private final String name;
+    private final String number;
+
+    public Building(int id, String name, String number) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
