2ab110150847f1346527ce509be6e2cad5f44673
JuanManuel030195 <manuelgarcia031@outlook.com>
2024-01-27 12:36:44 -0600

    AppState Enum added

---
+++ BarcodeSample1/app/src/main/java/com/symbol/barcodesample1/AppState.java
@@ -0,0 +1,10 @@
+package com.symbol.barcodesample1;
+
+public enum AppState {
+    NOT_LOGGED_IN,
+    LOGGED_IN,
+    VALIDATION_STARTED,
+    VALIDATION_ENDED,
+    ON_OLD_VALIDATIONS,
+    ON_OLD_VALIDATION;
+}
