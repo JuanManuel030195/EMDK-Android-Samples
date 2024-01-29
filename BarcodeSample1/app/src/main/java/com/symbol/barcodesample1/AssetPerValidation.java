27737818245c624eeb57078956ebddf064909b3a
Juan Manuel <GarciaRuizJuanManuel@JohnDeere.com>
2024-01-29 11:59:27 -0600

    assets per validation class aded

---
+++ BarcodeSample1/app/src/main/java/com/symbol/barcodesample1/AssetPerValidation.java
@@ -0,0 +1,55 @@
+package com.symbol.barcodesample1;
+
+import org.jetbrains.annotations.NotNull;
+
+public class AssetPerValidation {
+    private final int validationId;
+    private final String assetNumber;
+    private boolean scanned;
+    private ValidationStatus status;
+
+    public AssetPerValidation(int validationId, String assetNumber) {
+        this.validationId = validationId;
+        this.assetNumber = assetNumber;
+        this.scanned = false;
+        this.status = ValidationStatus.PENDING;
+    }
+
+    public int getValidationId() {
+        return validationId;
+    }
+
+    public String getAssetNumber() {
+        return assetNumber;
+    }
+
+    public boolean isScanned() {
+        return scanned;
+    }
+
+    public void setScanned(boolean scanned) {
+        this.scanned = scanned;
+    }
+
+    public ValidationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(
+        @NotNull Asset asset,
+        @NotNull Employee employee,
+        @NotNull Building building
+    ) {
+        if (!asset.getEmployeeNumber().equals(employee.getNumber())) {
+            this.status = ValidationStatus.WRONG_EMPLOYEE;
+            return;
+        }
+
+        if (!asset.getBuildingName().equals(building.getName())) {
+            this.status = ValidationStatus.WRONG_BUILDING;
+            return;
+        }
+
+        this.status = ValidationStatus.OK;
+    }
+}
