67a19e36abfe17044a3e7d199705ac77739807f8
JuanManuel030195 <manuelgarcia031@outlook.com>
2024-01-31 21:13:22 -0600

    test on new validation

--- BarcodeSample1/app/src/main/java/com/symbol/barcodesample1/MainActivity.java
+++ BarcodeSample1/app/src/main/java/com/symbol/barcodesample1/MainActivity.java
@@ -1119,7 +1119,7 @@ public class MainActivity extends Activity implements EMDKListener, DataListener
             public void run() {
                 Toast.makeText(
                         MainActivity.this,
-                        String.valueOf(localValidation.getBuilding().getId()),
+                        String.valueOf(localValidation.getBuilding().getName()),
                         Toast.LENGTH_LONG
                 ).show();
             }
